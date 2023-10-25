e32ffd5d00b477e8718e00e9adc648aef89d241d
Yunchen Zhang <yzhang39@scu.edu>
2023-10-25 16:16:05 -0700

    update login and signup

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React, { Component } from "react";
 import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Root from "./Root";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/home" element={<Home />} />
-            <Route exact path="/test" element= {<div>Hello world!</div>} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </BrowserRouter>
+       <Root>
+          <Switch>
+            <Route exact path="/home" component={Home} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+          </Switch>
+      </Root>
+
       </div>
     );
   }

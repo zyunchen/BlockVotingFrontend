8942ad8b7d13f73cc9212b84f2fd77712af8775a
yongkangchen <yongkangchen@Yongkangs-MacBook-Air.local>
2023-11-15 17:00:59 -0800

    add persistent redux

--- src/Root.js
+++ src/Root.js
@@ -4,6 +4,9 @@ import { Provider } from "react-redux";
 import { createBrowserHistory } from "history";
 import { applyMiddleware, createStore } from "redux";
 import { routerMiddleware, ConnectedRouter } from "connected-react-router";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react'
 
 import rootReducer from "./Reducer";
 
@@ -11,15 +14,26 @@ const Root = ({ children, initialState = {} }) => {
   const history = createBrowserHistory();
   const middleware = [thunk, routerMiddleware(history)];
 
+  const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+  const persistedReducer = persistReducer(persistConfig, rootReducer(history))
+
   const store = createStore(
-    rootReducer(history),
+    persistedReducer,
     initialState,
     applyMiddleware(...middleware)
   );
 
+  let persistor = persistStore(store)
+
   return (
     <Provider store={store}>
-      <ConnectedRouter history={history}>{children}</ConnectedRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <ConnectedRouter history={history}>{children}</ConnectedRouter>
+      </PersistGate>
     </Provider>
   );
 };
